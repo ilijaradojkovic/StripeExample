f4c89203745dd9f39b0dec59098a23fa3dd3c088
ilijaradojkovic <radojkovicika@gmail.com>
2023-01-15 15:12:24 +0100

    Added Update,GetAll,Search customer

--- src/main/java/com/example/stripeexample/controller/Controller.java
+++ src/main/java/com/example/stripeexample/controller/Controller.java
@@ -4,6 +4,8 @@ import com.example.stripeexample.entity.MyCustomer;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.Customer;
+import com.stripe.model.CustomerSearchResult;
+import com.stripe.param.CustomerSearchParams;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -42,6 +44,42 @@ public class Controller {
         Stripe.apiKey=stripeKey;
         Customer customer = Customer.retrieve(id);
         return customer.delete().toJson();
+    }
+
+    @PutMapping("/{id}")
+    public String updateCustomer(@PathVariable("id") String id,@RequestBody MyCustomer myCustomer) throws StripeException {
+        Stripe.apiKey=stripeKey;
+        Customer customer=Customer.retrieve(id);
+        Map<String, Object> params = new HashMap<>();
+        params.put("description",myCustomer.description());
+        params.put("email", myCustomer.email());
+        params.put("name", myCustomer.name());
+        params.put("phone", myCustomer.phoneNumber());
+        params.put("balance", myCustomer.balance());
+        return customer.update(params).toJson();
+    }
+
+    @GetMapping()
+    public String getAllCustomers(@RequestParam("limit") int limit) throws StripeException {
+        Stripe.apiKey=stripeKey;
+        Map<String, Object> params = new HashMap<>();
+        params.put("limit",limit);
+        return Customer.list(params).toJson();
+    }
+
+    @GetMapping("/search")
+    public String searchCustomers(@RequestParam("name") String name) throws StripeException {
+        Stripe.apiKey=stripeKey;
+        CustomerSearchParams params =
+                CustomerSearchParams
+                        .builder()
+                        //need custom string to be build "name:'fakename' AND metadata['foo']:'bar'
+                        .setQuery("name:'"+name+"'")
+                        .build();
+
+        CustomerSearchResult result = Customer.search(params);
+
+        return result.toJson();
     }
 
 }
