11e773636a56e9eca88f00e16a9017e9ec721b9d
Ilija <ilija.radojkovic@malkier.rs>
2023-01-18 10:45:39 +0100

    Added TransferController

--- src/main/java/com/example/stripeexample/controller/TransferController.java
+++ src/main/java/com/example/stripeexample/controller/TransferController.java
@@ -4,17 +4,15 @@ import com.example.stripeexample.entity.transfer.MyTransferObject;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.Transfer;
+import com.stripe.model.TransferCollection;
 import com.stripe.param.TransferCreateParams;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 //A Transfer object is created when you move funds between Stripe accounts as part of Connect.
-
+//Kada hocu ja sa mog akaunta da prebacim nesto na drugi connect akaunt
 @RestController
 @RequestMapping("/transfer")
 public class TransferController {
@@ -35,5 +33,24 @@ public class TransferController {
 
         Transfer transfer = Transfer.create(params);
         return transfer.toJson();
+    }
+    @GetMapping("/{id}")
+    public String retriveTransferObject(@PathVariable("id") String id) throws StripeException {
+        Stripe.apiKey = stripeKey;
+
+        Transfer transfer = Transfer.retrieve(id);
+
+        return  transfer.toJson();
+    }
+
+    @GetMapping
+    public String listTransfers() throws StripeException {
+        Stripe.apiKey = stripeKey;
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("limit", 3);
+
+        TransferCollection transfers = Transfer.list(params);
+        return  transfers.toJson();
     }
 }
